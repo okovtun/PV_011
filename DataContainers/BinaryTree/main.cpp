2fbb88b118ac757eb82928c8c5a46c190f061fac
Clayman <Clayman@DESKTOP-DM8E77U>
2021-06-04 21:34:39 +0300

    BinaryTree erase DONE

--- DataContainers/BinaryTree/main.cpp
+++ DataContainers/BinaryTree/main.cpp
@@ -19,14 +19,16 @@ class Tree
 #ifdef DEBUG
 			cout << "EConstructor:\t" << this << endl;
 #endif // DEBUG
-
 		}
 		~Element()
 		{
 #ifdef DEBUG
 			cout << "EDestructor:\t" << this << endl;
 #endif // DEBUG
-
+		}
+		bool is_leaf()const
+		{
+			return pLeft == pRight;
 		}
 		friend class Tree;
 	}*Root/*, someElement, *somePointer2element*/;	//Указатель на корневой элемент
@@ -44,12 +46,54 @@ public:
 		//Root = nullptr;
 		cout << "TConstructor:\t" << this << endl;
 	}
+	Tree(const initializer_list<int>& il) :Tree()
+	{
+		
+	}
 	~Tree()
 	{
 		clear(Root);
 		cout << "TDestructor:\t" << this << endl;
 	}
-	void insert(int Data, Element* Root)	//Здесь Element* Root - это указатель на ветку (поддерево)
+	void insert(const int& Data)
+	{
+		insert(Data, this->Root);
+	}
+	void erase(const int& Data)
+	{
+		erase(Data, Root);
+	}
+	int minValue()
+	{
+		return minValue(Root);
+	}
+	int maxValue()
+	{
+		return maxValue(Root);
+	}
+	int size()
+	{
+		return size(Root);
+	}
+	int sum()
+	{
+		return sum(Root);
+	}
+	double avg()
+	{
+		return avg(Root);
+	}
+	void print()
+	{
+		print(Root);
+		cout << endl;
+	}
+	void clear()
+	{
+		clear(Root);
+	}
+private:
+	void insert(const int& Data, Element* Root)	//Здесь Element* Root - это указатель на ветку (поддерево)
 	{
 		if (this->Root == nullptr)this->Root = new Element(Data);
 		if (Root == nullptr)return;
@@ -74,7 +118,35 @@ public:
 			else Root->pRight = new Element(Data);
 		}
 	}
-
+	void erase(const int& Data, Element*& Root)
+	{
+		if (Root == nullptr)return;
+		erase(Data, Root->pLeft);
+		erase(Data, Root->pRight);
+		if (Data == Root->Data)
+		{
+			if (Root->is_leaf())
+			{
+				delete Root;
+				Root = nullptr;
+			}
+			else
+			{
+				if (Root->pLeft)
+				{
+					Root->Data = maxValue(Root->pLeft);
+					erase(maxValue(Root->pLeft), Root->pLeft);
+				}
+				else
+				{
+					Root->Data = minValue(Root->pRight);
+					erase(minValue(Root->pRight), Root->pRight);
+				}
+			}
+		}
+		/*if(Root)erase(Data, Root->pLeft);
+		if(Root)erase(Data, Root->pRight);*/
+	}
 	int minValue(Element* Root)
 	{
 		/*if (Root->pLeft == nullptr)return Root->Data;
@@ -98,6 +170,10 @@ public:
 	{
 		return Root ? sum(Root->pLeft) + sum(Root->pRight) + Root->Data : 0;
 	}
+	double avg(Element* Root)
+	{
+		return (double)sum(Root) / size(Root);
+	}
 
 	void print(Element* Root)
 	{
@@ -106,12 +182,13 @@ public:
 		cout << Root->Data << tab;
 		print(Root->pRight);
 	}
-	void clear(Element* Root)
+	void clear(Element*& Root)
 	{
 		if (Root == nullptr)return;
 		clear(Root->pLeft);
 		clear(Root->pRight);
 		delete Root;
+		Root = nullptr;
 	}
 };
 
@@ -119,16 +196,42 @@ void main()
 {
 	setlocale(LC_ALL, "Russian");
 	int n;	//Количество элементов дерева
-	cout << "Введите количество элементов: "; cin >> n;
-	Tree tree;
-	for (int i = 0; i < n; i++)
+	//cout << "Введите количество элементов: "; cin >> n;
+	Tree tree = {50, 25, 16, 32, 64, 55, 77};
+	/*for (int i = 0; i < n; i++)
 	{
-		tree.insert(rand() % 100, tree.getRoot());
-	}
-	tree.print(tree.getRoot());
+		tree.insert(rand() % 100);
+	}*/
+	/*tree.insert(50);
+
+	tree.insert(25);
+	tree.insert(16);
+	tree.insert(32);
+	tree.insert(11);
+	tree.insert(18);
+	tree.insert(30);
+	tree.insert(36);
+	
+	tree.insert(64);
+	tree.insert(55);
+	tree.insert(80);
+	tree.insert(54);
+	tree.insert(58);
+	tree.insert(77);
+	tree.insert(85);*/
+
+	tree.print();
 	cout << endl;
-	cout << "Минимальное значение в дереве:  " << tree.minValue(tree.getRoot()) << endl;
-	cout << "Максимальное значение в дереве: " << tree.maxValue(tree.getRoot()) << endl;
-	cout << "Количество элементов в дереве:  " << tree.size(tree.getRoot()) << endl;
-	cout << "Сумма элементов дерева:         " << tree.sum(tree.getRoot()) << endl;
+	cout << "Минимальное значение в дереве:  " << tree.minValue() << endl;
+	cout << "Максимальное значение в дереве: " << tree.maxValue() << endl;
+	cout << "Количество элементов в дереве:  " << tree.size() << endl;
+	cout << "Сумма элементов дерева:         " << tree.sum() << endl;
+	cout << "Среднее арифметическое элементов дерева:" << tree.avg() << endl;
+	int value;
+	cout << "Введите удаляемое значение: "; cin >> value;
+	tree.erase(value);
+	tree.print();
+	tree.clear();
+	cout << tree.getRoot() << endl;
+	tree.print();
 }
