00257d9ad36a5bfa43467c7702c966e6af6519fa
Clayman <Clayman@DESKTOP-DM8E77U>
2021-06-10 21:52:06 +0300

    STL map

---
+++ STL/AssociativeContainers/main.cpp
@@ -0,0 +1,69 @@
+﻿#include<iostream>
+#include<string>
+#include<map>
+#include<list>
+//using std::cin;
+//using std::cout;
+//using std::endl;
+
+#define tab "\t"
+
+/*
+---------------------------------------------------
+Associative containers:map, set, multimap, multiset
+---------------------------------------------------
+*/
+
+#define MAP
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	//using namespace std;
+#ifdef MAP
+	//map - это ассоциативный контейнер, то есть, каждый элемент этого контейнера 
+	//хранит пару "ключ-значение" (key-value).
+	/*std::map<int, std::string> week =
+	{
+		std::pair<int, std::string>(0,"Sunday"),
+		std::pair<int, std::string>(1,"Monday"),
+		std::pair<int, std::string>(2,"Tuesday"),
+		std::pair<int, std::string>(3,"Wednesday"),
+		std::pair<int, std::string>(4,"Thursday"),
+		std::pair<int, std::string>(5,"Friday"),
+		std::pair<int, std::string>(8,"Thursday"),
+		std::pair<int, std::string>(6,"Saturday")
+	};
+	for (std::map<int, std::string>::iterator it = week.begin(); it != week.end(); it++)
+	{
+		std::cout << it->first << tab << it->second << std::endl;
+	}
+	std::cout << std::endl;*/
+
+	std::map<std::string, std::list<std::string>> dictionary =
+	{
+		std::pair<std::string, std::list<std::string>>("finite", {"ограниченный", "имеющий предел"}),
+		std::pair<std::string, std::list<std::string>>("adjacent", {"примыкающий", "смежный", "соседний"}),
+		std::pair<std::string, std::list<std::string>>("plot", {"карта", "план", "график", "сюжет"}),
+		std::pair<std::string, std::list<std::string>>("incident", {"случайность", "происшествие"}),
+		std::pair<std::string, std::list<std::string>>("splice", {"соединение", "сращивание"})
+	};
+	//std::cout << dictionary["finite"] << std::endl;
+	std::cout << dictionary["finite"].front() << std::endl;
+	dictionary["space"].assign({ "пространство", "космос", "пробел" });
+	for (std::map<std::string, std::list<std::string>>::iterator it = dictionary.begin(); it != dictionary.end(); it++)
+	{
+		std::cout << it->first << ":";
+		std::cout.width(20);
+		//std::cout << std::left;
+		for (std::list<std::string>::iterator jt = it->second.begin(); jt != it->second.end(); jt++)
+		{
+			std::cout << *jt << ", ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << dictionary.find("finite")->first << tab;;
+	for (std::string i : dictionary.find("finite")->second)std::cout << i << tab; std::cout << std::endl;
+#endif // MAP
+
+}
--- STL/Lists/main.cpp
+++ STL/Lists/main.cpp
@@ -1,10 +1,18 @@
 #include<iostream>
 #include<list>
+#include<forward_list>
 using std::cin;
 using std::cout;
 using std::endl;
 
 #define tab "\t"
+#define delimiter "\n------------------------------------------------------\n"
+
+//#define LIST
+//#define UNIQUE
+//#define LIST_MERGING
+
+#define FORWARD_LIST
 
 template<typename T>inline void list_properties(const std::list<T>& lst)
 {
@@ -13,6 +21,11 @@ template<typename T>inline void list_properties(const std::list<T>& lst)
 }
 template<typename T>void print_list(const std::list<T>& lst)
 {
+	if (lst.empty())
+	{
+		cout << "List is empty" << endl;
+		return;
+	}
 	for (std::list<int>::const_iterator it = lst.begin(); it != lst.end(); it++)
 	{
 		cout << *it << tab;
@@ -35,9 +48,10 @@ bool more_than_10(const int& value)
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef LIST
 	//list - это контейнер, который хранит данные в виде двусв€зного списка
 	cout << more_than_10 << endl;
-	std::list<int> lst = { 3, 5, 8, 13, 21};
+	std::list<int> lst = { 3, 5, 8, 13, 21 };
 	cout << "Empty list size: " << sizeof(list_wrapper) << endl;
 	list_properties(lst);
 	print_list(lst);
@@ -64,13 +78,48 @@ void main()
 	/*lst.resize(25);
 	lst.remove(0);
 	lst.remove_if(more_than_10);*/
+#ifdef UNIQUE
+	cout << delimiter << endl;
+	lst.sort();
 	for (int i : lst)cout << i << tab; cout << endl;
+	cout << delimiter << endl;
 	lst.unique();
 	for (int i : lst)cout << i << tab; cout << endl;
+	cout << delimiter << endl;
+	lst.reverse();
+	for (int i : lst)cout << i << tab; cout << endl;
+#endif // UNIQUE  
+#endif // LIST
 
+#ifdef LIST_MERGING
+	std::list<int> lst1 = { 3,5,8,13,21 };
+	std::list<int> lst2 = { 34,55,89 };
+	//lst1.merge(lst2);
+	lst2.merge(lst1);
+	print_list(lst1);
+	print_list(lst2);
+#endif // LIST_MERGING
 	/*for (int i = 0; i < lst.size(); i++)
 	{
 		cout << lst[i] << tab;
 	}
 	cout << endl;*/
+
+#ifdef FORWARD_LIST
+	std::forward_list<int> flst = { 3, 5, 8, 13, 21 };
+	flst.push_front(34);
+	for (std::forward_list<int>::iterator it = flst.begin(); it != flst.end(); it++)
+		cout << *it << tab;
+	cout << endl;
+
+	int index;
+	int value;
+	cout << "¬ведите инекс добавл€емого элемента: "; cin >> index;
+	cout << "¬ведите значение добавл€емого элемента: "; cin >> value;
+	std::forward_list<int>::iterator position = flst.before_begin();
+	for (int i = 0; i < index; i++)position++;
+	flst.insert_after(position, value);
+	for (int i : flst)cout << i << tab; cout << endl;
+#endif // FORWARD_LIST
+
 }
