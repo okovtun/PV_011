2d3c1805463235b4617ba3af31add6b8366ef074
Clayman <Clayman@DESKTOP-DM8E77U>
2021-08-27 21:53:42 +0300

    Exam

--- WinAPI/DialogControls/main.cpp
+++ WinAPI/DialogControls/main.cpp
@@ -1,6 +1,16 @@
 #include<Windows.h>
 #include"resource.h"
 
+CONST CHAR* variant[] =
+{
+	"This",
+	"is",
+	"my",
+	"first",
+	"List",
+	"Box"
+};
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -13,13 +23,31 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-	case WM_INITDIALOG: 
-		break;
+	case WM_INITDIALOG:
+	{
+		HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+		for (int i = 0; i < sizeof(variant) / sizeof(variant[0]); i++)
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)variant[i]);
+		SendMessage(hList, LB_SETCURSEL, 2, 0);
+	}
+	break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
 		case IDOK:
-			MessageBox(hwnd, "OK was pressed", "Info", MB_ICONINFORMATION | MB_OK);
+		{
+			const int SIZE = 256;
+			char sz_buffer[SIZE]{};
+			char sz_msg[SIZE]{};
+			strcpy(sz_msg, "Вы выбрали пункт: ");
+			
+			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+			INT i = SendMessage(hList, LB_GETCURSEL, 0, 0);
+			SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_buffer);
+			strcat(sz_msg, sz_buffer);
+			MessageBox(hwnd, sz_msg, "Info", MB_ICONINFORMATION | MB_OK);
+
+		}
 			break;
 		case IDCANCEL:
 			EndDialog(hwnd, 0);
